4470b6cf21075194825e92e2cab835466d78d8cb
Murugan <murugu2012@gmail.com>
2017-01-02 16:34:22 +0530

    Direct association between restaurants and questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   # Direct associations
 
+  belongs_to :restaurant
+
   # Indirect associations
 
   # Validations
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :questions,
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
