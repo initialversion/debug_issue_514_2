aa341b6d24540114d789e0bbcb01cece340df600
Murugan <murugu2012@gmail.com>
2017-01-02 16:35:10 +0530

    Integrate Ransack

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
   def index
-    @questions = Question.page(params[:page]).per(10)
+    @q = Question.ransack(params[:q])
+    @questions = @q.result(:distinct => true).includes(:restaurant, :responses).page(params[:page]).per(10)
 
     render("questions/index.html.erb")
   end
