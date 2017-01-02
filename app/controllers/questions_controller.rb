46747188521acf7165891744562a4894516e6b73
Murugan <murugu2012@gmail.com>
2017-01-02 16:34:54 +0530

    Add forms for new collection objects on show pages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,6 +6,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @response = Response.new
     @question = Question.find(params[:id])
 
     render("questions/show.html.erb")
