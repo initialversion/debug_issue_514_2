39e08f5da2baa6bd76155fea3298d55fa281a3cd
Murugan <murugu2012@gmail.com>
2017-01-02 16:34:51 +0530

    Whitelist columns in Question for ActiveAdmin

--- app/admin/question.rb
+++ app/admin/question.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Question do
 
+ permit_params :body, :option_1, :option_2, :name, :restaurant_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
